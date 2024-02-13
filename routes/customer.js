985fd3d616d889ad1fe193b5834ab5d0659ebb8d
navnath <nkwagh12@gmail.com>
2024-02-13 17:42:41 +0530

    customer delete functionality

--- routes/customer.js
+++ routes/customer.js
@@ -6,5 +6,6 @@ const customerController=require('../controller/customer');
 router.post('/addcustomer',customerController.addCustomer);
 router.get('/getCustomers',customerController.getAllCustomer);
 router.get('/getcustomer/:customerId',customerController.getCustomersById);
+router.delete('/deleteCustomer/:customerId',customerController.deleteCustomer);
 
 module.exports=router;
